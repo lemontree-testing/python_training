ac618e8e592736af953ed3f4833fc615f2a5247e
lemontree_testing <lemontreemail@gmail.com>
2015-05-20 23:27:21 +0500

    Тест на создание контактов.
    Тесты на создание контактов с использованием вспомогательных методов и
    классов.  Selenuim-based.

---
+++ contact.py
@@ -0,0 +1,12 @@
+__author__ = 'lemontree'
+
+class Contacts:
+    def __init__(self,name,lastname,nickname,company,email,day_of_birth, month_of_birth,year_of_birth):
+        self.firstname=name
+        self.lastname=lastname
+        self.nickname=nickname
+        self.company=company
+        self.email=email
+        self.day_of_birth=day_of_birth
+        self.month_of_birth=month_of_birth
+        self.year_of_birth=year_of_birth
