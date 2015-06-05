9fc39a29277bf587454260a13108071ec84e04b6
lemontree_testing <lemontreemail@gmail.com>
2015-06-05 10:23:41 +0500

    Оптимизированы переходы на страницы

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,6 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -22,7 +21,8 @@ class Application:
 
     def open_homepage(self, wd):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not(wd.current_url.endswith("/addressbook/")):
+            wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
